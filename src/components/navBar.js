6aa4f4fea633c442f5e68273ae9a159e455acf14
gokul <gokulramdevan@gmail.com>
2022-08-30 23:43:41 +0530

    commit

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,7 +5,7 @@ export function NavBar() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
         alt=""
       />
-      <h3>React</h3>
+      <h3>Reac</h3>
       <h4>React Course - Project</h4>
     </nav>
   );
